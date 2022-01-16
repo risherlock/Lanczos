6900c1aba1c1a1a2d960a883b5829e85ebb7dcc4
Rishav Dhungel <62413199+risherlock@users.noreply.github.com>
2022-01-16 22:27:29 +0545

    First commit

---
+++ lanczos/lanczos.h
@@ -0,0 +1,11 @@
+#ifndef _LANCZOS_H_
+#define _LANCZOS_H_
+
+// 0 to disable display
+#define DISPLAY_MATH 1
+
+#include "matrix.h"
+#include "vector.h"
+#include "quaternion.h"
+
+#endif // lanczos.h
